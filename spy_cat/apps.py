5d6ad7b1cbf72564565d9301fb4c77c60e6404e3
serhii <ockar2018@gmail.com>
2024-12-04 15:31:35 +0200

    refactor files with black

--- spy_cat/apps.py
+++ spy_cat/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SpyCatConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'spy_cat'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "spy_cat"
